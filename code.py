ff49e5fa3f21c027b43910f622b6b82ea489ab0b
gual14 <sobol.gal@gmail.com>
2022-06-10 17:35:58 +0300

    adding game over
    adding a game over statement(and maybe another fututres)

--- code.py
+++ code.py
@@ -8,34 +8,49 @@ from pygame.locals import (
     KEYDOWN,
     QUIT,
 )
+
 mat = [
-  [0,0,0,0,0],
-  [0,0,0,0,0],
-  [0,0,0,0,0],
-  [0,0,0,0,0],
-  [0,0,0,0,0],
-  [0,0,0,0,0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
 ]
-def placematrix(i,j):
-  #insx = round(x//100)
-  #insy = round(y//100)
-  mat[i][j] = 1
+
+
+def placematrix(i, j):
+    # insx = round(x//100)
+    # insy = round(y//100)
+    mat[i][j] = 1
+
 
 def cube_draw(i, j):
-    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(j*100+5, i*100+5, 90, 90))
+    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(j * 100 + 5, i * 100 + 5, 90, 90))
+
 
 def print_matrix(matrix):
-  for i in matrix:
-    print(*i)
-  print()
+    for i in matrix:
+        print(*i)
+    print()
+
+
 pygame.init()
 
-def isStop(i,j):
+
+def isStop(i, j):
     return i >= 5 or mat[i + 1][j] == 1
-def contgame(i,j):
-    placematrix(i,j)
+
+
+def isGameOver():
+    return mat[0][2] == 1
+
+
+def contgame(i, j):
+    placematrix(i, j)
     print_matrix(mat)
-    #shuld summon new block
+    # shuld summon new block
+
 
 screen = pygame.display.set_mode([500, 600])
 i = 0
@@ -50,12 +65,12 @@ while running:
             if event.key == K_ESCAPE:
                 running = False
             if event.key == K_LEFT:
-                if j > 0 and mat[i][j-1] == 0:
-                    #x = x - 100
+                if j > 0 and mat[i][j - 1] == 0:
+                    # x = x - 100
                     j = j - 1
             if event.key == K_RIGHT:
-                if j < 4 and mat[i][j+1] == 0:
-                    #x = x + 100
+                if j < 4 and mat[i][j + 1] == 0:
+                    # x = x + 100
                     j = j + 1
     screen.fill((0, 0, 0))
 
@@ -63,20 +78,23 @@ while running:
         for temp_j in range(len(mat[temp_i])):
             if mat[temp_i][temp_j] == 1:
                 cube_draw(temp_i, temp_j)
-                
+
     cube_draw(i, j)
     if i < 9:
         if z == 750:
-            if isStop(i,j):
-                contgame(i,j)
-                #running = False
+            if isStop(i, j):
+                contgame(i, j)
+                # running = False
                 i = 0
                 j = 2
-            #y = y + 100
-            i = i + 1
+            else:
+                i = i + 1
             z = 0
         z = z + 1
 
     pygame.display.flip()
+    if isGameOver():
+        running = False
+        print("Game Over")
 
 pygame.quit()
